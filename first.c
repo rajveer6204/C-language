8001ca5cb410f224480f0fa8c1e29307b15c3895
Rajveer <singhveerraj232003@gmail.com>
2025-05-12 03:05:13 +0530

    update new thing

--- first.c
+++ first.c
@@ -1,5 +1,7 @@
-#include<stdio.h>
-int main(){
-    printf("hello");
-    return 0;
+#include <stdio.h>
+int main() {
+    int a; // Declare integer variable 'a'
+    a = 9; // Assign value 9 to 'a'
+    printf ("The value of a is %d\n", a); 
+    return 0;      
 }

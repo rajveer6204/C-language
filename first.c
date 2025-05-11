f588400039d57d07352a645a258182774eaf4222
Rajveer <singhveerraj232003@gmail.com>
2025-05-12 03:48:06 +0530

    problem and solve

--- first.c
+++ first.c
@@ -1,7 +1,16 @@
-#include <stdio.h>
+// #include <stdio.h>
+// int main() {
+//     int a; // Declare integer variable 'a'
+//     a = 9; // Assign value 9 to 'a'
+//     printf ("The value of a is %d\n", a); 
+//     return 0;      
+// }
+
+#include<stdio.h>
+
 int main() {
-    int a; // Declare integer variable 'a'
-    a = 9; // Assign value 9 to 'a'
-    printf ("The value of a is %d\n", a); 
-    return 0;      
+    int a ;
+    scanf("%d", &a);
+    printf("The value of a is %d\n", a);
+    return 0;
 }
---
+++ probleam.c
@@ -0,0 +1,17 @@
+/*question :
+1. Write a C program to calculate aree of a rectangle 
+a. Using hard coded inputs. 
+b. Using inputs supplied by the user. 
+2. Colculate the area of a circte and modify the same program to calculate the volume of a cylinder given its radius and height. 
+3. Writo a program to convert Celsius (Centigrade degrees temperature to Fahrenheit). 
+4. Write a program to calculate simple interest for a set of values representing principal, number of years and rate of interest.*/
+
+#include <stdio.h>
+int main() {
+    int leanth;
+    int breadth;
+    scanf("%d", &breadth);
+    scanf("%d", &leanth);
+    printf("Area of rectangle: %d\n", leanth * breadth);
+    return 0;
+}
